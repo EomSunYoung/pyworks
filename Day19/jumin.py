09b926c77a7fd1e9c44e035f6f4465fd5c82caa1
eomsunyoung <kuroich123@naver.com>
2021-09-06 13:18:43 +0900

    Day19 업로드

---
+++ Day19/jumin.py
@@ -0,0 +1,31 @@
+# 3
+pin = "881120-1068234"
+
+yyyymmdd = pin[:6]
+num = pin[7:]
+
+print("19" + yyyymmdd)
+print(num)
+
+# 4 성별 문자가 1이면 '남자', 아니면 '여자'
+print(pin[7])
+if pin[7] == '1' or pin[7] == '3':
+    print('남자')
+else:
+    print('여자')
+
+# 5
+a = "a:b:c:d"
+b = a.replace(':', '#') # 이전 문자, 새 문자
+print(b)
+
+# 6
+a = [1, 3, 5, 4, 2]
+a.sort()
+a.reverse()
+print(a)
+
+# 7 리스트 -> 문자열
+a = ['Life', 'is', 'too', 'short.']
+result = ' '.join(a)
+print(result)
---
+++ Day19/scorepass.py
@@ -0,0 +1,40 @@
+# 학생의 시험 점수 합격 판단
+# 조건이 60점 이상이면 '합격', 아니면 '불합격'
+
+score = [90, 45, 67, 59, 85]
+idx = 0
+
+print("for ~ in 리스트 반복문")
+for i in score:
+    idx += 1
+    if i >= 60:
+        print(str(idx) + "번 학생은 합격입니다.")
+    else:
+        print(str(idx) + "번 학생은 불합격입니다.")
+    # print(i)
+print()
+
+print("for ~ in range 반복문")
+n = len(score)
+for i in range(0, n):
+    if  score[i] >= 60:
+        print(str(i+1) + "번 학생은 합격입니다.")
+    else:
+        print(str(i+1) + "번 학생은 불합격입니다.")
+print('='*20)
+
+# 성적의 합계, 평균
+total = 0
+avg = 0.0
+
+for i in score:
+    total += i
+
+avg = total / len(score)
+
+# 평균 = 합개 / 개수
+print("합계 : " + str(total) + "점")
+print("평균 : " + str(avg) + "점")
+
+
+
